8b2f025d290dded9c7cfefacd388d22f8e0f79b3
Alejandro Hernandez Gutierrez <alejandro.hernandez.gutierrez@intel.com>
2022-08-29 09:36:17 -0500

    Añadiendo el cero negativo

--- Tarea_1.py
+++ Tarea_1.py
@@ -8,12 +8,13 @@ def newton_raphson(ejercicio):
     iterations = 1
 
     print("================================================")
+    print(f"Ecuacion: {ec}")
     while True:
         # Evaluar ambas expresiones con el nuevo valor de x_n
         ec_ev, ec_der_ev = ec.evalf(subs={x:x_n}) , ec_der.evalf(subs={x:x_n})
 
         if (ec_der_ev == 0):
-            print("Si la derivada es 0, se indetermina la división")
+            print("La derivada es 0, se indetermina la division")
             break
 
         x_n_new = x_n - (ec_ev/ec_der_ev)
@@ -22,7 +23,7 @@ def newton_raphson(ejercicio):
         if (x_n_new == x_n) or (iterations > 100):
             exactitud = abs(0-ec_ev)
             if(exactitud<E):
-                print(f"Solución para {ec}: {x_n_new}")
+                print(f"Solucion para {ec}: {x_n_new}")
                 print(f"Iteraciones: {iterations}")
                 print(f"La Exactitud:{exactitud} cumple con el criterio establecido (es menor que {E})")
                 break
@@ -38,21 +39,21 @@ def newton_raphson(ejercicio):
 
 x = sp.Symbol("x")
 E = 10**-4 # Error
-# Definición de las funciones
+# Definicion de las funciones
 f1 = x**3 - 2*x**2 - 5
 f2 = x - cos(x)
 f3 = x - 0.8 - 0.2*sin(x)
 f4 = ln(x - 1) + cos(x - 1)
 f5 = exp(x) - 3*x**2
-# Encuentre una aproximación de raíz de 5 correcta con exactitud 10-4.
+# Encuentre una aproximacion de raiz de 5 correcta con exactitud 10-4.
 f6 = x**2 - 5
-# Encuentre el único cero negativo de f(x) = ln(x^2+1)-e^(0.4x)*cos(pi*x)
+# Encuentre el unico cero negativo de f(x) = ln(x^2+1)-e^(0.4x)*cos(pi*x)
 # con exactitud 10-6.
 f7 = ln(x**2+1)-exp(0.4*x)*cos(sp.pi*x)
 
-# for (función, x_0, cifras_sig, Error) in ejercicios
+# for (funcion, x_0, cifras_sig, Error) in ejercicios
 ejercicios = [(f1, 3, 10, E), (f2, 1, 10, E), (f3, 1, 10, E), (f4, 1.4, 10, E), (f5, 1, 10, E)]
-ejercicios.extend([(f6, 2.3, 10, E), (f7, .5, 10, 10**-6)])
+ejercicios.extend([(f6, 2.3, 10, E), (f7, -0.27, 10, 10**-6)])
 
 for ejercicio in ejercicios:
     newton_raphson(ejercicio)
