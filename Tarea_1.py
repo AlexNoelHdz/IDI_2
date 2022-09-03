0cfc82862ad50a59c6682d38db493ca0e0535895
Alejandro Hernandez Gutierrez <alejandro.hernandez.gutierrez@intel.com>
2022-09-03 15:59:27 -0500

    Añadiendo clase Ejercicio a Tarea 1

--- Tarea_1.py
+++ Tarea_1.py
@@ -1,40 +1,46 @@
 import sympy as sp
 from sympy import *
 
-def newton_raphson(ejercicio):
-    ec, x_n, cifras_sig, E = ejercicio
-    # Derivada de la ec
-    ec_der = diff(ec)
-    iterations = 1
+class Ejercicio:
+    def __init__(self, ec, x_n, cifras_sig, E ):
+        self.ec = ec
+        self.x_n = x_n
+        self.cifras_sig = cifras_sig
+        self.E = E
 
-    print("================================================")
-    print(f"Ecuacion: {ec}")
-    while True:
-        # Evaluar ambas expresiones con el nuevo valor de x_n
-        ec_ev, ec_der_ev = ec.evalf(subs={x:x_n}) , ec_der.evalf(subs={x:x_n})
+    def newton_raphson(self):
+        ec, x_n, cifras_sig, E  = self.ec, self.x_n, self.cifras_sig, self.E
+        # Derivada de la ec
+        ec_der = diff(ec)
+        iterations = 1
 
-        if (ec_der_ev == 0):
-            print("La derivada es 0, se indetermina la division")
-            break
+        print("================================================")
+        while True:
+            # Evaluar ambas expresiones con el nuevo valor de x_n
+            ec_ev, ec_der_ev = ec.evalf(subs={x:x_n}) , ec_der.evalf(subs={x:x_n})
 
-        x_n_new = x_n - (ec_ev/ec_der_ev)
-        print(f"x_n: {x_n} x_n_new: {x_n_new}")
-        if (x_n_new == x_n) or (iterations > 100):
-            x_n_new = Float(x_n_new,cifras_sig)
-            ec_ev = ec.evalf(subs={x:x_n})
-            exactitud = abs(0-ec_ev)
-            if(exactitud<E):
-                print(f"Solucion para {ec}: {x_n_new}")
-                print(f"Iteraciones: {iterations}")
-                print(f"La Exactitud:{exactitud} cumple con el criterio establecido (es menor que {E})")
+            if (ec_der_ev == 0):
+                print("La derivada es 0, se indetermina la division")
                 break
-            else:
-                print(f"Exactitud:{exactitud} no cumple con el criterio establecido (menor que {E})")
-                break
-        iterations+=1
-        x_n = x_n_new
+            x_n_new = x_n - (ec_ev/ec_der_ev)
+            # print(f"x_n: {x_n} x_n_new: {x_n_new}")
+            if (x_n_new == x_n) or (iterations > 100):
+                x_n_new = Float(x_n_new,cifras_sig)
+                ec_ev = ec.evalf(subs={x:x_n})
+                exactitud = abs(0-ec_ev)
+                if(exactitud<E):
+                    print(f"Solucion para {ec}: {x_n_new}")
+                    print(f"Iteraciones: {iterations}")
+                    print(f"La Exactitud:{exactitud} cumple con el criterio establecido (es menor que {E})")
+                    break
+                else:
+                    print(f"{ec} tiene exactitud: {exactitud}, no cumple con el criterio establecido (menor que {E})")
+                    break
+            iterations+=1
+            x_n = x_n_new
 
 x = sp.Symbol("x")
+
 E = 10**-4 # Error
 # Definicion de las funciones
 f1 = x**3 - 2*x**2 - 5
@@ -48,9 +54,15 @@ f6 = x**2 - 5
 # con exactitud 10-6.
 f7 = ln(x**2+1)-exp(0.4*x)*cos(sp.pi*x)
 
-# for (funcion, x_0, cifras_sig, Error) in ejercicios
-ejercicios = [(f1, 3, 10, E), (f2, 1, 10, E), (f3, 1, 10, E), (f4, 1.4, 10, E), (f5, 1, 10, E)]
-ejercicios.extend([(f6, 2.3, 10, E), (f7, -0.27, 10, 10**-6)])
+ej1 = Ejercicio(f1, 3, 10, E)
+ej2 = Ejercicio(f2, 1, 10, E)
+ej3 = Ejercicio(f3, 1, 10, E)
+ej4 = Ejercicio(f4, 1.4, 10, E)
+ej5 = Ejercicio(f5, 1, 10, E)
+ej6 = Ejercicio(f6, 2.3, 10, E)
+ej7 = Ejercicio(f7, -0.27, 10, 10**-6)
 
-for ejercicio in ejercicios:
-    newton_raphson(ejercicio)
+ejercicios = [ej1,ej2,ej3,ej4,ej5,ej6,ej7]
+# for (funcion, x_0, cifras_sig, Error) in ejercicios
+for ej in ejercicios:
+    ej.newton_raphson()
