81db5434e4eaa831d6e9c6db3f743b83cbef1566
Alejandro Hernandez Gutierrez <alejandro.hernandez.gutierrez@intel.com>
2022-09-04 13:46:49 -0500

    Mostrando resultados más claros

--- Tarea_2.py
+++ Tarea_2.py
@@ -6,8 +6,8 @@ import string
 class Ejercicio:
     def __init__(self, funciones, x_i_dic):
         self.funciones = Matrix(funciones)
-        self.x_i = Matrix(list(x_i_dic.values()))
         self.cifras_sig = 5
+        self.x_i = Matrix(list(x_i_dic.values())).evalf(self.cifras_sig)
         self.E  = 10**-4 # Error
         self.variables = list(self.funciones.atoms(Symbol))
 
@@ -16,31 +16,24 @@ class Ejercicio:
         x_i = self.x_i
         J_inv = self.funciones.jacobian(self.variables).inv()
         J_det = self.funciones.jacobian(self.variables).det()
-        print("================================================")
+        print(f"================================================")
         while True:
+            print(f"x_i: {x_i}")
             subs_dic = dict(zip(self.variables, x_i))
-            # subs_dic = {key:Float(value, self.cifras_sig) for (key,value) in subs_dic.items()}
             J_det_ev = J_det.subs(subs_dic)
             if (J_det_ev == 0):
-                print("El determinante del Jacobiano es 0, no s posible realizar operaciones")
+                print("El determinante del Jacobiano es 0, no es posible realizar operaciones")
                 break
-
-            J_inv_eval = J_inv.subs(subs_dic)
             functions_ev = self.funciones.subs(subs_dic)
-
-            x_i_new = x_i - (J_inv_eval * functions_ev)
-
-            print(f"x_n: {x_i} x_n_new: {x_i_new}")
-
             norm_2 = sum(abs(functions_ev))
             cumple_exactitud = norm_2 < self.E
             if(cumple_exactitud):
-                    print(f"Solucion para {self.funciones}: {subs_dic}")
-                    print(f"Iteraciones: {iterations}")
-                    print(f"La Norma 2:{norm_2} cumple con el criterio establecido (es menor que {self.E})")
+                    print(f"Solucion para {self.funciones}: {subs_dic}. Iteraciones: {iterations}")
+                    print(f"La Norma 2: {norm_2} cumple con el criterio establecido (es menor que {self.E})")
                     break
-            else:
-                x_i = x_i_new.evalf(self.cifras_sig)
+
+            J_inv_eval = J_inv.subs(subs_dic)
+            x_i = (x_i - (J_inv_eval * functions_ev)).evalf(self.cifras_sig)
             iterations+=1
 
 # Definicion de las funciones
@@ -55,7 +48,7 @@ ej2 = Ejercicio(f2, {x:0.7,y:1.0})
 ej3 = Ejercicio(f3, {x:1.0,y:2.0})
 ej4 = Ejercicio(f4, {x:1.6,y:2.6})
 ej5 = Ejercicio(f5, {x:1.0,y: 1.0, z:1.0})
-ejercicios = [ej1] #,ej2,ej3,ej4]
-# for (funcion, x_0, cifras_sig, Error) in ejercicios
+ejercicios = [ej1,ej2,ej3,ej4,ej5]
+
 for ej in ejercicios:
     ej.newton_raphson()
