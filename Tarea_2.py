90484ee38a06bec2aa105b140f5f1dfdfa5234cb
Alejandro Hernandez Gutierrez <alejandro.hernandez.gutierrez@intel.com>
2022-09-05 12:32:34 -0500

    Resolviendo problema de extraer variables de Sympy

--- Tarea_2.py
+++ Tarea_2.py
@@ -8,7 +8,7 @@ class Ejercicio:
         self.cifras_sig = 5
         self.x_i = Matrix(list(x_i_dic.values())).evalf(self.cifras_sig)
         self.E  = 10**-4 # Error
-        self.variables = list(self.funciones.atoms(Symbol))
+        self.variables =  list(x_i_dic.keys()) # list(self.funciones.atoms(Symbol))
 
     def newton_raphson(self):
         iteraciones = 1
@@ -36,16 +36,16 @@ class Ejercicio:
             x_i = (x_i - (J_inv_eval * functions_ev)).evalf(self.cifras_sig)
             iteraciones+=1
             # Forma alternativa de cortar el ciclo. Se alcanza cuando
-            # 1. Bug de Sympy desordena x,y,z... y rompe con los valores iniciales
-            # 2. No se ha llegado a la respuesta y x_i esta creciendo y decreciendo. (No llegara)
-            if iteraciones > 90:
+            # 1. No se ha llegado a la respuesta y x_i esta creciendo y decreciendo. (No llegara)
+            if iteraciones >= 20:
+                print(f"Estado actual {self.funciones}:\n{subs_dic}. \nIteraciones: {iteraciones}")
                 break
 
 f1 = [x**2+y-1, x-2*y**2]
 f2 = [x**2-10*x+y**2+5, x*y**2+x-10*y+8]
 f3 = [x*sin(y)-1, x**2+y**2-4]
 f4 = [y**2*ln(x)-3, y-x**2]
-f5 = [x+y-z-2, x**2+y, -y**2+1-1]
+f5 = [x+y-z-2, x**2+y, z-y**2-1]
 
 ej1_r1 = Ejercicio("Ejercicio 1 raiz 1", f1, {x:0.7,y:0.6})
 ej1_r2 = Ejercicio("Ejercicio 1 raiz 2", f1, {x:1.36,y:-0.83})
@@ -60,7 +60,7 @@ ej3_r4 = Ejercicio("Ejercicio 3 raiz 4", f3, {x:-2.0,y:-0.6})
 
 ej4_r1 = Ejercicio("Ejercicio 4 raiz 1", f4, {x:1.6,y:2.6})
 
-ej5_r1 = Ejercicio("Ejercicio 5 raiz 1", f5, {x:1.0,y: 1.0, z:1.0})
+ej5_r1 = Ejercicio("Ejercicio 5 raiz 1", f5, {x:0.9,y:-0.9,z:1.9})
 
 for ej in [ej1_r1, ej1_r2, ej2_r1, ej2_r2, ej3_r1, ej3_r2, ej3_r3, ej3_r4, ej4_r1, ej5_r1]:
     ej.newton_raphson()
@@ -122,23 +122,4 @@ x_i: Matrix([[1.5934], [2.5389]])
 x_i: Matrix([[1.5931], [2.5381]])
 Solucion para Matrix([[y**2*log(x) - 3], [-x**2 + y]]): {x: 1.5931, y: 2.5381}. Iteraciones: 3
 La Norma 2: 0.0000038147 cumple con el criterio establecido (es menor que 0.0001)
-=====================Ejercicio 5 raiz 1=====================
-x_i: Matrix([[1.0000], [1.0000], [1.0000]])
-x_i: Matrix([[0.25000], [-1.2500], [0.50000]])
-x_i: Matrix([[-0.37500], [-2.1250], [0.25000]])
-x_i: Matrix([[-0.020833], [-1.8958], [0.12500]])
-x_i: Matrix([[1.4896], [-0.44790], [0.062500]])
-x_i: Matrix([[0.73431], [-1.2344], [0.031250]])
-x_i: Matrix([[0.35652], [-1.6279], [0.015625]])
-x_i: Matrix([[0.16730], [-1.8249], [0.0078125]])
-x_i: Matrix([[0.071977], [-1.9241], [0.0039063]])
-x_i: Matrix([[0.022421], [-1.9756], [0.0019531]])
-x_i: Matrix([[-0.010568], [-2.0096], [0.00097656]])
-x_i: Matrix([[0.017819], [-1.9817], [0.00048828]])
-x_i: Matrix([[0.0020587], [-1.9977], [0.00024414]])
-x_i: Matrix([[-0.028618], [-2.0285], [0.00012207]])
-x_i: Matrix([[-0.013243], [-2.0132], [6.1035e-5]])
-x_i: Matrix([[-0.0054690], [-2.0054], [3.0518e-5]])
-Solucion para Matrix([[x + y - z - 2], [x**2 + y], [-y**2]]): {x: -0.0054690, z: -2.0054, y: 3.0518e-5}. Iteraciones: 16
-La Norma 2: 0.000064243 cumple con el criterio establecido (es menor que 0.0001)
 """
