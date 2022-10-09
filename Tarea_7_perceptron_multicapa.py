c117267ec4d52b3d9ce98fec99923055ba3dfdd1
Alejandro Hernandez Gutierrez <alejandro.hernandez.gutierrez@intel.com>
2022-10-09 14:09:27 -0500

    Log resultados file

--- Tarea_7_perceptron_multicapa.py
+++ Tarea_7_perceptron_multicapa.py
@@ -1,15 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import datetime
 from pandas import read_excel
 from math import inf
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 from sympy import re
 from functools import wraps
+np.random.seed = 666
 MIN_ERROR = 10**-6
 MAX_EPOCAS = 100000
 
 def print_duration(func):
+    """Decorador de Python para imprimir la duracion de un metodo
+    """
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
         start_time = time.perf_counter()
@@ -19,6 +23,18 @@ def print_duration(func):
         print(f'Función {func.__name__} toma {total_time:.4f} seconds')
         return result
     return timeit_wrapper
+
+def log_resultados_ejercicio(log_list):
+    """Escribe la lista de resultados en un archivo de texto
+
+    Args:
+        log_list (list): Resultados
+    """
+    ct = datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+    path = f"logs/{ct}_results.txt"
+    with open(path, 'w') as fp:
+        fp.write('\n'.join(log_list))
+    print(f"resultados: {path}")
 
 class Perceptron:
     def __init__(self, df, numero_entradas, numero_salidas, por_test, alpha, L):
@@ -38,16 +54,17 @@ class Perceptron:
         n_entrenamiento = int(len(df)*por_test)
         datos_entrenamiento = df[:n_entrenamiento]
         datos_prueba = df[n_entrenamiento:]
+        self.log = []
 
         w_h, w_o, E, epocas = self.train_data(N, M, L, datos_entrenamiento, alpha)
 
         y_pred = self.test_data(datos_prueba, N, w_h, w_o)
 
-        self.print_results(E, N,epocas, y_pred, datos_prueba, "Probando con Datos de prueba",0)
+        self.log_results(E, N,epocas, y_pred, datos_prueba, "Probando con Datos de prueba",0)
 
         y_pred = self.test_data(datos_entrenamiento, N, w_h, w_o)
 
-        self.print_results(E,N,epocas, y_pred, datos_entrenamiento, "Probando con Datos de entrenamiento",(n_entrenamiento-len(df)))
+        self.log_results(E,N,epocas, y_pred, datos_entrenamiento, "Probando con Datos de entrenamiento",(len(df)-n_entrenamiento))
 
     def sigmoid(self, x, a = 1):
         return 1/(1+np.e**(-a*x))
@@ -94,7 +111,7 @@ class Perceptron:
             y_res[j] = y.reshape(len(y),)
         return y_res
 
-    def print_results(self, error, N, epocas, y_pred, data_real, nombre, offset):
+    def log_results(self, error, N, epocas, y_pred, data_real, nombre, offset):
             y_real = data_real[:,N:]
             y_pred_round = np.round(y_pred.astype(float))
             y_real = y_real.astype(float)
@@ -103,25 +120,33 @@ class Perceptron:
             recall = recall_score(y_real, y_pred_round)
             f1 = f1_score(y_real, y_pred_round)
             accuracy = accuracy_score(y_real, y_pred_round)
-            print(f'================{nombre}================')
-            print("[y_pred]|[y_real]. ([y_pred_original])")
+            self.log.append(f'================{nombre}================')
+            self.log.append(f'Error: {error}, Epocas:{epocas}.')
+            self.log.append(f'Accuracy: {round(accuracy,4)}. Precision: {round(precision,4)}. Recall: {round(recall,4)}. F1: {round(f1,4)} ')
+            self.log.append(f'Matriz Confusion:\n{matriz_confusion}')
+            self.log.append("[y_pred]|[y_real]. ([y_pred_original])")
             n_datos = len(y_pred_round)
             for i in range(n_datos):
-                print(f"{i+1+offset:0>4}: {y_pred_round[i]}    {y_real[i]}. ({y_pred[i]})")
+                self.log.append(f"{i+offset:0>3}: {y_pred_round[i]}    {y_real[i]}. ({y_pred[i]})")
 
-            print(f'Error: {error}, Epocas:{epocas}.')
-            print(f'Accuracy: {round(accuracy,4)}. Precision: {round(precision,4)}. Recall: {round(recall,4)}. F1: {round(f1,4)} ')
-            print(f'Matriz Confusión:\n{matriz_confusion}')
-
+# Desarrollo del ejercicio de la Tarea
+# Obtencion de datos
 df = np.array(read_excel('data/PercMultAplicado.xlsx'))
 np.random.shuffle(df)
 # En este caso, los datos normalizados comienzan en la posición 8
-df = df[:,8:]
+df_normalizado= df[:,8:]
 
-Perceptron(
-    df=df,
+ejercicio = Perceptron(
+    df=df_normalizado,
     numero_entradas=3,
     numero_salidas=1,
     por_test=0.7,
     alpha=1,
     L=3*2)
+
+# Extendemos el log con los datos originales enumerados
+ejercicio.log.append("Datos Originales: ")
+for i in range(len(df)):
+    ejercicio.log.append(f"{i:0>3}: {df[i]}")
+
+log_resultados_ejercicio(ejercicio.log)
